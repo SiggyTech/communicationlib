241ee31c18e605fe83d608d3faf6969290dcfd11
francisco00 <francisco00@gmail.com>
2019-10-14 01:03:20 -0300

    Add +

--- notificatorlib/src/main/java/com/siggytech/utils/notificatorlib/Conf.java
+++ notificatorlib/src/main/java/com/siggytech/utils/notificatorlib/Conf.java
@@ -5,7 +5,8 @@ package com.siggytech.utils.notificatorlib;
  */
 
 public class Conf {
-    public static String SERVER_IP = "http://192.168.1.148:7777";
+    //public static String SERVER_IP = "http://192.168.1.148:7777";//local
+    public static String SERVER_IP = "http://35.247.219.199:7777";//cloud
     static int SERVER_PORT = 27001;
     public static String SERVER_LISTENER= "http://192.168.1.148";
     static int LOCAL_PORT = 1984;
