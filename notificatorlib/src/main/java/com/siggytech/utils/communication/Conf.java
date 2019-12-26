50012523a125c83e5ef6b405439b68a60b7fda4a
francisco00 <francisco00@gmail.com>
2019-12-26 11:53:17 -0300

    PTT OK, Notifications OK

--- notificatorlib/src/main/java/com/siggytech/utils/communication/Conf.java
+++ notificatorlib/src/main/java/com/siggytech/utils/communication/Conf.java
@@ -1,8 +1,8 @@
 package com.siggytech.utils.communication;
 
 public class Conf {
-    //public static String SERVER_IP = "192.168.1.148";//local
-    public static String SERVER_IP = "35.247.219.199";//cloud
+
+    public static String SERVER_IP = "";//cloud
 
     public static int SERVER_PORT = 7778;
     public static int SERVER_WS_PORT = 8080;
